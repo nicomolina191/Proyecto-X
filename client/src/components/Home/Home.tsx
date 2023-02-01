ae6f2e4d5bc33a93c0eedb6dcfe9105f1f68f197
Nico Molina <nicomolina191@hotmail.com>
2023-02-01 17:54:54 -0300

    Primer commit

---
+++ client/src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import style from './Home.module.css';
+import React from 'react';
+
+export default function Home() {
+
+    return (
+        <div className='Home'>
+            <h1 className={style.titulo}>Este es el home del proyecto pa</h1>
+        </div>
+    )
+
+}
