a8c2c2ea8cc828fca295a0f93e944d2029b602bb
pablocana <pablocanavesi@hotmail.com>
2023-02-15 05:03:01 -0300

    cambios navbar
    cambios navbar
    cambios css

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -8,24 +8,36 @@ export default function Navbar() {
     return (
         <div className={style.navbar}>
             <img src={logo} alt="logo" className={style.img}/>
-            <Link className={style.link} to='/'>
-                Home
-            </Link>
-            <Link className={style.link} to='/shop'>
-                Shop
-            </Link>
-            <Link className={style.link} to='/about'>
-                About us
-            </Link>
-            <Link className={style.link} to='/contact'>
-                Contact us
-            </Link>
-            <Link className={style.link} to='/login'>
-                Sign in
-            </Link>
-            <Link className={style.link} to='/register'>
-                Sign up
-            </Link>
+            <span className={style.cont_link}>
+                <Link className={style.link} to='/'>
+                    Home
+                </Link>
+            </span>
+            <span className={style.cont_link}>
+                <Link className={style.link} to='/shop'>
+                    Shop
+                </Link>
+            </span>
+            <span className={style.cont_link}>
+                <Link className={style.link} to='/about'>
+                    About us
+                </Link>
+            </span>
+            <span className={style.cont_link}>
+                <Link className={style.link} to='/contact'>
+                    Contact us
+                </Link>
+            </span>
+            <span className={style.cont_link}>
+                <Link className={`${style.link} ${style.in}`} to='/login'>
+                    Sign in
+                </Link>
+            </span>
+            <span className={style.cont_link}>
+                <Link className={`${style.link} ${style.in}`} to='/register'>
+                    Sign up
+                </Link>
+            </span>
         </div>
     )
 
