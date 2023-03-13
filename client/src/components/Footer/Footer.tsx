66badd7e43afda7dc669cef4f1c032dd64004ada
Nico Molina <nicomolina191@hotmail.com>
2023-03-13 20:46:33 -0300

    Agrego animaciones y mejoro un poco la estetica

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import style from './Footer.module.css';
 import { Link } from 'react-router-dom';
+import img from '../../assets/logo1.png';
 
 export default function Footer() {
 
     return (
         <div className={style.footer}>
-            <Link className={style.link} to='/'>
-                Home
-            </Link>
-            <Link className={style.link} to='/shop'>
-                Shop
-            </Link>
-            <Link className={style.link} to='/about'>
-                About us
-            </Link>
-            <Link className={style.link} to='/contact'>
-                Contact us
-            </Link>
+            <div className={style.image}>
+                <img src={img} alt="logo" className={style.img}/>
+            </div>
+            <div className={style.links}>
+                <Link className={style.link} to='/'>
+                    Home
+                </Link>
+                <Link className={style.link} to='/shop'>
+                    Shop
+                </Link>
+                <Link className={style.link} to='/about'>
+                    About us
+                </Link>
+                <Link className={style.link} to='/contact'>
+                    Contact us
+                </Link>
+            </div>
         </div>
     )
 }
--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import Section1 from '../Section1/Section1';
@@ -6,17 +6,77 @@ import Section2 from '../Section2/Section2';
 import Section3 from '../Section3/Section3';
 import Section4 from '../Section4/Section4';
 import Section5 from '../Section5/Section5';
+import ScrollReveal from 'scrollreveal';
+import './Home.css';
 
 export default function Home() {
 
+    useEffect(() =>{
+        ScrollReveal().reveal('.box1', {
+            origin: 'right',
+            delay: 375,
+            duration: 1000,
+            distance: '500px',
+            scale: 1,
+            easing: 'ease',
+            reset: true
+        })
+        ScrollReveal().reveal('.box2', {
+            origin: 'right',
+            delay: 375,
+            duration: 1000,
+            distance: '500px',
+            scale: 1,
+            easing: 'ease',
+            reset: true
+        })
+        ScrollReveal().reveal('.box3', {
+            origin: 'right',
+            delay: 375,
+            duration: 1000,
+            distance: '500px',
+            scale: 1,
+            easing: 'ease',
+            reset: true
+        })
+        ScrollReveal().reveal('.box4', {
+            origin: 'right',
+            delay: 375,
+            duration: 1000,
+            distance: '500px',
+            scale: 1,
+            easing: 'ease',
+            reset: true
+        })
+        ScrollReveal().reveal('.box5', {
+            origin: 'right',
+            delay: 375,
+            duration: 1000,
+            distance: '500px',
+            scale: 1,
+            easing: 'ease',
+            reset: true
+        })
+    }, [])
+
     return (
-        <div className='Home'>
+        <div className='home'>
             <Navbar/>
-            <Section1/>
-            <Section2/>
-            <Section3/>
-            <Section4/>
-            <Section5/>
+            <div className='box1'>
+                <Section1/>
+            </div>
+            <div className='box2'>
+                <Section2/>
+            </div>
+            <div className='box3'>
+                <Section3/>
+            </div>
+            <div className='box4'>
+                <Section4/>
+            </div>
+            <div className='box5'>
+                <Section5/>
+            </div>
             <Footer/>
         </div>
     )
